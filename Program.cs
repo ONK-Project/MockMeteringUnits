451c2b89a838d1d33e575c1246b7c19c12e4252e
MLR <lyngrosenquistmorten@gmail.com>
2020-04-03 17:21:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MockMeteringsUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve all sensors
+
+            // Make 3 lists of metering unit guids 
+
+            List<long> heatMeteringUnitIds;
+            List<long> waterMeteringUnitIds;
+            List<long> electricityMeteringUnitIds;
+
+            // Fill the 3 lists dependent of the type of sensor retrieved
+
+            // Start 3 threads with different delay doing different submissions
+            var starts = new ThreadStarts();
+
+            var heatThread = new Thread(new ThreadStart(starts.HeatSubmission));
+            heatThread.Start();
+
+            var waterThread = new Thread(new ThreadStart(starts.WaterSubmission));
+            waterThread.Start();
+
+            var electrictyThread = new Thread(new ThreadStart(starts.ElectricitySubmission));
+            electrictyThread.Start();
+        }
+    }
+}
---
+++ ThreadStarts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MockMeteringsUnits
+{
+    public class ThreadStarts
+    {
+        const int generalFrequency = 900_000; // 15 minutes 
+        const int waterFrequency = generalFrequency;
+        const int heatFrequency = 4*generalFrequency;
+        const int electricityFrequency = 8 * generalFrequency;
+        
+        public void HeatSubmission()
+        {
+            Thread.Sleep(heatFrequency);
+        }
+
+        public void WaterSubmission()
+        {
+            Thread.Sleep(waterFrequency);
+        }
+
+        public void ElectricitySubmission()
+        {
+            Thread.Sleep(electricityFrequency);
+        }
+    }
+}
